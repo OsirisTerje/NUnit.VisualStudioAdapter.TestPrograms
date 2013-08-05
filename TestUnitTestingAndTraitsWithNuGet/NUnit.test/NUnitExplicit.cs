04a81b9919bcfdd8561a049f0ee45933f4030863
Terje Sandstrom <terje@osiris.no>
2013-08-05 17:06:21 +0200

    added tests for explicit attributes

---
+++ TestUnitTestingAndTraitsWithNuGet/NUnit.test/NUnitExplicit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit.test
+{
+    [Category("Exp")]
+    [Explicit]
+    [TestFixture]
+    public class NUnitExplicit1
+    {
+        [Test]
+        [Category("CI")]
+        [Property("Priority", "2")]
+        [Property("Owner", "Terje")]
+        [Property("Runningtime", "Short")]
+        public void TestMethodEN1()
+        {
+            var sut = new SomeClasses.VerySimpleMath();
+            int result = sut.Add(2, 3);
+            Assert.AreEqual(result, 5);
+        }
+
+        [Test]
+        [Property("Priority", "1")]
+        public void TestMethodEN2()
+        {
+            var sut = new SomeClasses.VerySimpleMath();
+            int result = sut.Add(2, 3);
+            Assert.AreEqual(result, 5);
+        }
+    }
+
+    [TestFixture]
+    public class NUnitExplicit2
+    {
+        [Test]
+        [Category("CI")]
+        [Property("Priority", "2")]
+        [Property("Owner", "Terje")]
+        [Property("Runningtime", "Short")]
+        public void TestMethodEN21()
+        {
+            var sut = new SomeClasses.VerySimpleMath();
+            int result = sut.Add(2, 3);
+            Assert.AreEqual(result, 5);
+        }
+
+        [Test]
+        [Explicit]
+        [Property("Priority", "1")]
+        public void TestMethodEN22()
+        {
+            var sut = new SomeClasses.VerySimpleMath();
+            int result = sut.Add(2, 3);
+            Assert.AreEqual(result, 5);
+        }
+    }
+
+}
