f4c0361bc6436baafdfa9a331d4d3d03503d627c
Terje Sandstrom <terje@osiris.no>
2013-08-10 09:48:18 +0200

    file renaming

---
+++ TestUnitTestingAndTraitsWithNuGet/UnitTestProject1/DerivedClassTestMS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DerivedClassTestMS
+{
+    [TestClass]
+    abstract public class MSUnitTestBase
+    {
+        [TestMethod]
+        public void BaseTestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+    [TestClass]
+    public class MSUnitTestDerived : MSUnitTestBase
+    {
+        [TestMethod]
+        public void DerivedTestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+
+}
