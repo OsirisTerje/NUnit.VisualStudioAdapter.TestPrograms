82e1bcc24febd691efd87e925bde728e1536c957
Terje Sandstrom <terje@osiris.no>
2013-08-22 01:11:28 +0200

    Added multitest

---
+++ TestUnitTestingAndTraitsWithNuGet/UnitTestProject1/MultiTestInSameClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MultiTest
+{
+    [NUnit.Framework.TestFixture]
+    [TestClass]
+    public class MultiInSameClass
+    {
+        [TestMethod]
+        public void MethodMS1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [NUnit.Framework.Test]
+        public void MethodN1()
+        {
+            NUnit.Framework.Assert.IsTrue(true);
+        }
+
+    }
+}
