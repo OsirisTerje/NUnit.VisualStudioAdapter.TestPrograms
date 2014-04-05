284f4b5df01cc4e9a3eeee7708addec4f6e7fc34
Terje Sandstrom <terje@hermit.no>
2014-04-06 00:22:23 +0200

    Added test for random issue, working

---
+++ TestUnitTestingAndTraitsWithNuGet/Issue15/Random.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Issue15
+{
+    public class SUT
+    {
+    }
+
+
+    [TestFixture]
+    public class TestSUTForRandomIssue
+    {
+        
+    }
+
+}
---
+++ TestUnitTestingAndTraitsWithNuGet/Issue15/WorkingRandomAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Issue15
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class AnyAttribute : ParameterDataAttribute
+    {
+        public override IEnumerable GetData(ParameterInfo parameter)
+        {
+            if (parameter.ParameterType == typeof(int))
+            {
+                return new[] { 122 };
+            }
+            else
+            {
+                return new[] { "a" };
+            }
+        }
+    }
+
+    public class Class1234
+    {
+        [Test]
+        public void ShouldBehavior([Any] int a, [Any] string b)
+        {
+            Assert.Fail("FAILED!");
+        }
+    }
+}
