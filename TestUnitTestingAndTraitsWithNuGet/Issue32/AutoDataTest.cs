c9f277e046efaf1bed81da192165c05e054b05b7
Terje Sandstrom <terje@hermit.no>
2014-04-17 07:42:35 +0200

    Intial working code for issue32

---
+++ TestUnitTestingAndTraitsWithNuGet/Issue32/AutoDataTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.NUnit2;
+
+namespace Issue32
+{
+
+    public interface I
+    { }
+
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            : base(new Fixture().Customize(new AutoMoqCustomization()))
+        { }
+    }
+
+
+    [TestFixture]
+    public class TestClass
+    {
+        [Theory, AutoMoqData]
+        public void TestI(I i)
+        {
+            
+        }
+    }
+}
---
+++ TestUnitTestingAndTraitsWithNuGet/Issue32/LocalAddin.cs
@@ -0,0 +1,9 @@
+using NUnit.Core.Extensibility;
+
+namespace Issue32
+{
+    [NUnitAddin]
+    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    {   
+    }
+}
