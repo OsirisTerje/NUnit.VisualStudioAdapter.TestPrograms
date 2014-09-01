086b8b8a9c1453b025c0cb330968f400eb7e93a2
Terje Sandstrom <terje@hermit.no>
2014-09-01 21:51:19 +0200

    Added test for issue 43

---
+++ TestUnitTestingAndTraitsWithNuGet/UnitTestProject1/Issue43.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+
+// Tests for Issue #43
+namespace MultiTest
+{
+    [TestFixture(typeof(int))]
+    public class GenericTests<T>
+    {
+        [Test]
+        public void GenericTest1()
+        { }
+
+        [Test]
+        public void GenericTest2()
+        { }
+    }
+
+
+    [TestFixture]
+    public class NonGenericTests
+    {
+        [Test]
+        public void NonGenericTest1()
+        { }
+
+        [Test]
+        public void NonGenericTest2()
+        { }
+    }
+
+
+}
